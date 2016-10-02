125fe3c62d483ee5db3fe8baa6397b857915fd18
spinuvlad <spinu.vlad@gmail.com>
2016-10-02 17:04:51 +0300

    dsd

---
+++ _info/cmd_terminal_sublime_vim/Sublime Text 3_UBUNTU.c
@@ -0,0 +1,7 @@
+Sublime Text 3 : UBUNTU
+
+1. CTRL + ~ in proga Sublime
+www.google.com --> package control sublime text 3 --> primul link --> copy text --> paste in prog. Sublime Text 3 in terminalul care apare dupa dupa tastele CTRL + ~ 
+
+2. CTRL + SHIFT + P --> install (Install Package) -->c99 --> ss:c99
+
